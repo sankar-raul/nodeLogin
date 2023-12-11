4556f789e8c1a998ea8c8f7ea57c70881ea2f4c8
sankar-coding <raulsankar99@gmail.com>
2023-12-11 20:06:20 +0530

    Initial commit

---
+++ node.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+/*app.get("/",(req,res) => {
+	res.send("<h1>Hello</h1>");
+});*/
+
+app.set("view engine","ejs");
+
+app.get("/",(req,res) => {
+    res.render("index");
+});
+
+app.get("/users/register",(req,res) => {
+    res.render("register");
+});
+
+app.get("/users/login",(req,res) => {
+    res.render("login");
+});
+
+app.get("/users/dashboard",(req,res) => {
+    res.render("dashboard",{user: "Sankar"});
+});
+
+app.listen(PORT,() => {
+       console.log(`Server is running on port ${PORT}`);
+});
