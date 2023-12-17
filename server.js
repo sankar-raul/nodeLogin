564941124feaeade56cc0ed1f34819ec9e383eac
sankar-coding <raulsankar99@gmail.com>
2023-12-17 12:47:37 +0530

    debug

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
- const {pool} = require('dbConfig');
+ const {pool} = require('./dbConfig');
 
 /*app.get("/",(req,res) => {
 	res.send("<h1>Hello</h1>");
