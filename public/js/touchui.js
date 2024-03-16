1198ef46224f120d2914df6ffb01e15cfec48cea
sankar-coding <raulsankar99@gmail.com>
2024-03-16 20:40:32 +0530

    😁

--- public/js/touchui.js
+++ public/js/touchui.js
@@ -1,14 +1,25 @@
-const elements = document.getElementsByClassName("touch-ui");
-for (let bt of elements) {
-bt.addEventListener("click",(e) => {
-    let x = e.pageX - e.target.getBoundingClientRect().left;
-    let y = e.pageY - e.target.getBoundingClientRect().top;
-    let cir = document.createElement("span");
-    cir.classList.add("circle");
-    cir.style.marginTop = x + "px";
-    cir.style.marginLeft = y + "px"; 
-    console.log(x + " " + y);
-    e.currentTarget.appendChild(cir);
-    setTimeout(() => cir.remove(),400);
-});
-}
+const buttons = document.getElementsByClassName("touch-ui");
+
+
+buttons.forEach(button => {
+    button.addEventListener('click', function (e) {
+        const x = e.pageX
+        const y = e.pageY
+
+        const buttonTop = e.target.offsetTop
+        const buttonLeft = e.target.offsetLeft
+
+        const xInside = x - buttonLeft
+        const yInside = y - buttonTop
+
+        const circle = document.createElement('span')
+        circle.classList.add('circle')
+        circle.style.top = yInside + 'px'
+        circle.style.left = xInside + 'px'
+
+        this.appendChild(circle)
+
+        setTimeout(() => circle.remove(), 500)
+    })
+})
+    
