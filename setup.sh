ed92f03e963c1e78a6e3a4b38663e09456a5d75e
sankar-coding <raulsankar99@gmail.com>
2024-03-23 00:01:02 +0530

    s

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
 apt install postgresql
+pg_ctl initdb -D ./db
+pg_ctl -D ./db start
+psql postgres -c "CREATE USER dbuser WITH PASSWORD 'password' SUPERUSER;"
+psql postgres -c "CREATE TABLE users (id BIGSERIAL,email VARCHAR(30),name VARCHAR(30),password TEXT)"
+pg_ctl -D ./db stop
 npm install
 npm i nodemon -g
